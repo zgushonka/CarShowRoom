21202745ec0a8f49a8e5fdae35401ce6dbbb8f7a
Oleksandr Buravlyov <Oleksandr.Buravlyov@symphony-solutions.eu>
2018-04-09 18:18:35 +0300

    minor project reorganization

--- ShowRoom/Networking/URLParameters.swift
+++ ShowRoom/Networking/URLParameters.swift
@@ -10,10 +10,10 @@ import Foundation
 
 struct ManufacureURLParameters {
     static func make(page: Int, pageSize: Int, key: String?) -> [String:Any] {
-        var parameters: [String:Any] = [ParameterKey.page.rawValue: page,
-                                        ParameterKey.pageSize.rawValue: pageSize]
+        var parameters: [String:Any] = [Parameter.page.rawValue: page,
+                                        Parameter.pageSize.rawValue: pageSize]
         if let _ = key {
-            parameters[ParameterKey.key.rawValue] = key
+            parameters[Parameter.key.rawValue] = key
         }
         return parameters
     }
@@ -21,13 +21,20 @@ struct ManufacureURLParameters {
 
 struct CarsURLParameters {
     static func make(manufacturer: Int, page: Int, pageSize: Int, key: String?) -> [String:Any] {
-        var parameters: [String:Any] = [ParameterKey.manufacturer.rawValue: manufacturer,
-                                        ParameterKey.page.rawValue: page,
-                                        ParameterKey.pageSize.rawValue: pageSize]
+        var parameters: [String:Any] = [Parameter.manufacturer.rawValue: manufacturer,
+                                        Parameter.page.rawValue: page,
+                                        Parameter.pageSize.rawValue: pageSize]
         if let _ = key {
-            parameters[ParameterKey.key.rawValue] = key
+            parameters[Parameter.key.rawValue] = key
         }
         return parameters
     }
+}
+
+enum Parameter: String {
+    case manufacturer
+    case page
+    case pageSize
+    case key = "wa_key"
 }
 
